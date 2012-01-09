41daafa8e30496a731d48f8126f99788db8d4965
Kevin Ruland <kruland@sunflower.com>
2012-01-09 19:00:19 +0000

    Get rid of timed splash and display a better welcome screen.

--- android/src/net/sf/openrocket/android/Main.java
+++ android/src/net/sf/openrocket/android/Main.java
@@ -6,47 +6,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.View;
-import android.widget.ImageView;
+import android.view.Window;
 
 public class Main extends Activity {
 
 	private static final int PICK_ORK_FILE_RESULT = 1;
-
-	private static final int STOPSPLASH = 0;
-	//time in milliseconds
-	private static final long SPLASHTIME = 3000;
-
-	private ImageView splash;
-
-	//handler for splash screen
-	private Handler splashHandler = new Handler() {
-		/* (non-Javadoc)
-		 * @see android.os.Handler#handleMessage(android.os.Message)
-		 */
-		@Override
-		public void handleMessage(Message msg) {
-			switch (msg.what) {
-			case STOPSPLASH:
-				//remove SplashScreen from view
-				splash.setVisibility(View.GONE);
-				break;
-			}
-			super.handleMessage(msg);
-		}
-	};
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
 		setContentView(R.layout.main);
-		splash = (ImageView) findViewById(R.id.splashscreen);
-		Message msg = new Message();
-		msg.what = STOPSPLASH;
-		splashHandler.sendMessageDelayed(msg, SPLASHTIME);
 	}
 
 	/* (non-Javadoc)
