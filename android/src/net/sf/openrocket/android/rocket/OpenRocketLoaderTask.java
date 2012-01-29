510adee8c7fcdbb734754f67e32a0970bcded831
Sampo Niskanen <sampo.niskanen@iki.fi>
2012-01-29 18:15:33 +0000

    motor loading refactoring

--- android/src/net/sf/openrocket/android/rocket/OpenRocketLoaderTask.java
+++ android/src/net/sf/openrocket/android/rocket/OpenRocketLoaderTask.java
@@ -3,13 +3,14 @@ package net.sf.openrocket.android.rocket;
 import java.io.File;
 
 import net.sf.openrocket.document.OpenRocketDocument;
+import net.sf.openrocket.file.DatabaseMotorFinder;
 import net.sf.openrocket.file.RocketLoadException;
 import net.sf.openrocket.file.openrocket.importt.OpenRocketLoader;
 import android.os.AsyncTask;
 import android.util.Log;
 
 public class OpenRocketLoaderTask extends AsyncTask<File, Void, OpenRocketLoaderResult> {
-
+	
 	private final static String TAG = "OpenRocketLoaderTask";
 	
 	/* (non-Javadoc)
@@ -17,21 +18,20 @@ public class OpenRocketLoaderTask extends AsyncTask<File, Void, OpenRocketLoader
 	 */
 	@Override
 	protected OpenRocketLoaderResult doInBackground(File... arg0) {
-		Log.d(TAG,"doInBackgroud");
+		Log.d(TAG, "doInBackgroud");
 		
 		OpenRocketLoader rocketLoader = new OpenRocketLoader();
 		try {
 			OpenRocketLoaderResult result = new OpenRocketLoaderResult();
-			OpenRocketDocument rocket = rocketLoader.load(arg0[0]);
+			OpenRocketDocument rocket = rocketLoader.load(arg0[0], new DatabaseMotorFinder());
 			result.rocket = rocket;
 			result.warnings = result.warnings;
 			return result;
-		}
-		catch( RocketLoadException ex ) {
+		} catch (RocketLoadException ex) {
 			Log.e(TAG, "doInBackground rocketLaoder.load threw", ex);
 		}
 		return null;
 		
 	}
-
+	
 }
