0621c1971d4cad2cbe3497b03c5b47c8d2a86a57
Bill Kuker <bkuker@billkuker.com>
2012-07-01 14:47:30 +0000

    Some 3d test junk, to be deleted later

---
+++ core/3d-Test-Junk/net/sf/openrocket/startup/EditTest.java
@@ -0,0 +1,113 @@
+package net.sf.openrocket.startup;
+import javax.swing.JFrame;
+
+import net.sf.openrocket.database.ComponentPresetDatabase;
+import net.sf.openrocket.database.ThrustCurveMotorSetDatabase;
+import net.sf.openrocket.document.OpenRocketDocument;
+import net.sf.openrocket.file.DatabaseMotorFinder;
+import net.sf.openrocket.file.openrocket.importt.OpenRocketLoader;
+import net.sf.openrocket.gui.configdialog.NoseConeConfig;
+import net.sf.openrocket.gui.configdialog.RocketComponentConfig;
+import net.sf.openrocket.gui.util.GUIUtil;
+import net.sf.openrocket.gui.util.SwingPreferences;
+import net.sf.openrocket.l10n.ResourceBundleTranslator;
+import net.sf.openrocket.rocketcomponent.NoseCone;
+import net.sf.openrocket.rocketcomponent.RocketComponent;
+import net.sf.openrocket.startup.Application;
+import net.sf.openrocket.startup.ConcurrentComponentPresetDatabaseLoader;
+import net.sf.openrocket.startup.ExceptionHandler;
+
+/**
+ * An application for quickly testing 3d figure witout all the OpenRocket user
+ * interface
+ * 
+ * @author bkuker
+ * 
+ */
+public class EditTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		GUIUtil.setBestLAF();
+		Application.setExceptionHandler(new ExceptionHandler() {
+
+			@Override
+			public void uncaughtException(Thread thread, Throwable throwable) {
+				throwable.printStackTrace();
+
+			}
+
+			@Override
+			public void handleErrorCondition(Throwable exception) {
+				exception.printStackTrace();
+			}
+
+			@Override
+			public void handleErrorCondition(String message, Throwable exception) {
+				exception.printStackTrace();
+
+			}
+
+			@Override
+			public void handleErrorCondition(String message) {
+				System.err.println(message);
+
+			}
+		});
+		Application.setBaseTranslator(new ResourceBundleTranslator("l10n.messages"));
+		Application.setMotorSetDatabase(new ThrustCurveMotorSetDatabase(false) {
+			{
+				startLoading();
+			}
+
+			@Override
+			protected void loadMotors() {
+			}
+		});
+		Application.setPreferences(new SwingPreferences());
+
+		// Must be done after localization is initialized
+		ComponentPresetDatabase componentPresetDao = new ComponentPresetDatabase(true) {
+
+			@Override
+			protected void load() {
+				ConcurrentComponentPresetDatabaseLoader presetLoader = new ConcurrentComponentPresetDatabaseLoader( this );
+				presetLoader.load();
+				try {
+					presetLoader.await();
+				} catch ( InterruptedException iex) {
+					
+				}
+			}
+			
+		};
+		componentPresetDao.load("datafiles", ".*csv");
+		Application.setComponentPresetDao(componentPresetDao);
+
+		OpenRocketDocument doc = new OpenRocketLoader().loadFromStream(
+				EditTest.class.getResourceAsStream("/datafiles/examples/Clustered rocket design.ork"),
+				new DatabaseMotorFinder());
+
+		JFrame ff = new JFrame();
+		ff.setSize(1200, 400);
+		ff.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		NoseCone nc = null;
+		for (RocketComponent c : doc.getDefaultConfiguration()) {
+			if (c instanceof NoseCone) {
+				nc = (NoseCone) c;
+			}
+		}
+
+		RocketComponentConfig ncc = new RocketComponentConfig(doc, nc);
+
+		JFrame jf = new JFrame();
+		jf.setSize(600, 400);
+		jf.setContentPane(ncc);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setVisible(true);
+
+	}
+}
---
+++ core/3d-Test-Junk/net/sf/openrocket/startup/Quick3dMain.java
@@ -0,0 +1,84 @@
+package net.sf.openrocket.startup;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import net.sf.openrocket.database.ComponentPresetDatabase;
+import net.sf.openrocket.database.ThrustCurveMotorSetDatabase;
+import net.sf.openrocket.document.OpenRocketDocument;
+import net.sf.openrocket.file.DatabaseMotorFinder;
+import net.sf.openrocket.file.openrocket.importt.OpenRocketLoader;
+import net.sf.openrocket.gui.main.componenttree.ComponentTree;
+import net.sf.openrocket.gui.scalefigure.RocketPanel;
+import net.sf.openrocket.gui.util.SwingPreferences;
+import net.sf.openrocket.l10n.ResourceBundleTranslator;
+import net.sf.openrocket.startup.Application;
+
+/**
+ * An application for quickly testing 3d figure witout all the OpenRocket user interface
+ * 
+ * @author bkuker
+ *
+ */
+public class Quick3dMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		Application.setBaseTranslator(new ResourceBundleTranslator(
+				"l10n.messages"));
+		Application.setMotorSetDatabase(new ThrustCurveMotorSetDatabase(false) {
+			{
+				startLoading();
+			}
+
+			@Override
+			protected void loadMotors() {
+			}
+		});
+		Application.setPreferences(new SwingPreferences());
+		
+		// Must be done after localization is initialized
+		ComponentPresetDatabase componentPresetDao = new ComponentPresetDatabase(true) {
+
+			@Override
+			protected void load() {
+				ConcurrentComponentPresetDatabaseLoader presetLoader = new ConcurrentComponentPresetDatabaseLoader( this );
+				presetLoader.load();
+				try {
+					presetLoader.await();
+				} catch ( InterruptedException iex) {
+					
+				}
+			}
+			
+		};
+		componentPresetDao.load("datafiles", ".*csv");
+		Application.setComponentPresetDao( componentPresetDao );
+
+		OpenRocketDocument doc = new OpenRocketLoader().loadFromStream(
+				Quick3dMain.class.getResourceAsStream("/datafiles/examples/Clustered rocket design.ork"),
+				new DatabaseMotorFinder());
+
+		JFrame ff = new JFrame();
+		ff.setSize(1200, 400);
+		ff.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		RocketPanel panel;
+
+		panel = new RocketPanel(doc);
+
+		ComponentTree ct = new ComponentTree(doc);
+		panel.setSelectionModel(ct.getSelectionModel());
+
+		JPanel p = new JPanel();
+		p.setLayout(new BorderLayout());
+		p.add(ct, BorderLayout.WEST);
+		p.add(panel, BorderLayout.CENTER);
+		ff.setContentPane(p);
+		ff.setVisible(true);
+	}
+}
