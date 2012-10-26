4c84b3dad76420cfd663448e1bc5f751adb9e5a2
bkuker <bkuker@billkuker.com>
2012-10-26 14:24:00 -0400

    Provide Markers class

---
+++ core/src/net/sf/openrocket/logging/Markers.java
@@ -0,0 +1,8 @@
+package net.sf.openrocket.logging;
+
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
+
+public class Markers {
+	public static final Marker USER_MARKER = MarkerFactory.getMarker("User");
+}
