7cdd139afee455324b9459a9ade55fab3223ecd1
obechris <obechriss@yahoo.com>
2014-01-08 16:04:22 -0800

    added openrocket core.src.OpenRocketAPI.java it should resemble the final pieces that we need. contains  RunSimulation  loadRocket	(simplified)  getflightdata getters
    all in java if python code can call this we should be ok.

---
+++ core/src/OpenRocketAPI.java
@@ -0,0 +1,68 @@
+import net.sf.openrocket.*;
+import net.sf.openrocket.simulation.*;
+import net.sf.openrocket.simulation.exception.SimulationException;
+
+
+public class OpenRocketAPI {
+	
+	private FlightData m_CFlightData;
+	private SimulationConditions m_CSimulationConditions;
+	private SimulationStatus m_CStatus;
+	
+	public int LoadRocket(String szFileName) {
+		return loadorkfile(szFileName); //this needs to be more complex...
+	}
+	
+	public void RunSimulation(){
+	
+	SimulationEngine boink = new BasicEventSimulationEngine; 
+	
+	try {
+		m_CFlightData= boink.simulate(m_CSimulationConditions);
+	} catch (SimulationException e) {
+		// TODO Auto-generated catch block
+		System.out.println("oops RunSimulation threw an error");
+	}
+	
+};
+	
+	public double getMaxAltitude() {
+		return m_CFlightData.getMaxAltitude();
+	}
+	
+	public double getMaxVelocity() {
+		return m_CFlightData.getMaxVelocity();
+	}
+	
+	public double getMaxAcceleration() {
+		return m_CFlightData.getTimeToApogee();
+	}
+	
+	public double getMaxMachNumber() {
+		return m_CFlightData.getMaxMachNumber();
+	}
+	
+	public double getTimeToApogee() {
+		return m_CFlightData.getTimeToApogee();
+	}
+	
+	public double getFlightTime() {
+		return m_CFlightData.getFlightTime();
+	}
+	
+	public double getGroundHitVelocity() {
+		return m_CFlightData.getGroundHitVelocity();
+	}
+	
+	public double getLaunchRodVelocity() {
+		return m_CFlightData.getLaunchRodVelocity();
+	}
+	
+	public double getDeploymentVelocity() {
+		return m_CFlightData.getDeploymentVelocity();
+	}
+	
+	private int loadorkfile(String filename) {
+	}
+	
+};
