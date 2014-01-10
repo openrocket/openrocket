dc5676c2a2d3732409a6b05cd17cdd14b2ab5bac
obechris <obechriss@yahoo.com>
2014-01-09 17:10:57 -0800

    added some stuff to load rocket and run simulation have not tested.

--- core/src/OpenRocketAPI.java
+++ core/src/OpenRocketAPI.java
@@ -1,30 +1,57 @@
+import java.io.File;
+
+import net.sf.openrocket.document.OpenRocketDocument;
+import net.sf.openrocket.file.GeneralRocketLoader;
+import net.sf.openrocket.file.RocketLoadException;
+import net.sf.openrocket.simulation.BasicEventSimulationEngine;
 import net.sf.openrocket.simulation.FlightData;
 import net.sf.openrocket.simulation.SimulationConditions;
-import net.sf.openrocket.simulation.SimulationStatus;
+import net.sf.openrocket.simulation.SimulationEngine;
+import net.sf.openrocket.simulation.SimulationOptions;
+import net.sf.openrocket.simulation.exception.SimulationException;
 
 
 public class OpenRocketAPI {
 	
 	private FlightData m_CFlightData;
 	private SimulationConditions m_CSimulationConditions;
-	private SimulationStatus m_CStatus;
+	
+	//private SimulationStatus m_CStatus; //will be used later.
 	
 	public int LoadRocket(String szFileName) {
-		
-		return loadorkfile(szFileName); //this needs to be more complex...
-		
+		try {
+			File Filename = new File(szFileName);
+			GeneralRocketLoader test = new GeneralRocketLoader(Filename);
+			OpenRocketDocument temp = test.load();
+			if (temp.getSimulationCount() > 0)
+			{
+				SimulationOptions temp2 = temp.getSimulation(0).getSimulatedConditions();
+				if (temp2 != null)
+				{
+					m_CSimulationConditions = temp2.toSimulationConditions();
+				}
+			}
+			//return loadorkfile(szFileName); //this needs to be more complex...
+		} catch (RocketLoadException oops) {
+			System.out.print("made a mistake file : ");
+			System.out.print(szFileName);
+			System.out.print(oops.toString());
+			return 1;
+			
+		}
+		return 0;
 	}
 	
 	public void RunSimulation() {
-		/*SimulationEngine boink = new BasicEventSimulationEngine; 
+		SimulationEngine boink = new BasicEventSimulationEngine();
 		
 		try {
-		m_CFlightData= boink.simulate(m_CSimulationConditions);
+			m_CFlightData = boink.simulate(m_CSimulationConditions);
 		} catch (SimulationException e) {
-		// TODO Auto-generated catch block
-		System.out.println("oops RunSimulation threw an error");
+			// TODO Auto-generated catch block
+			System.out.println("oops RunSimulation threw an error");
 		}
-		*/
+		
 	};
 	
 	public double getMaxAltitude() {
@@ -50,7 +77,6 @@ public class OpenRocketAPI {
 	public double getFlightTime() {
 		return m_CFlightData.getFlightTime();
 	}
-	
 	
 	public double getGroundHitVelocity() {
 		return m_CFlightData.getGroundHitVelocity();
