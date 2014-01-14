1dbb9ff82d1a7e0eb8bbca5ecbef989b60e79366
obechris <obechriss@yahoo.com>
2014-01-13 17:57:05 -0800

    added some things you can actually call

--- core/src/OpenRocketAPI.java
+++ core/src/OpenRocketAPI.java
@@ -9,20 +9,43 @@ import net.sf.openrocket.simulation.SimulationConditions;
 import net.sf.openrocket.simulation.SimulationEngine;
 import net.sf.openrocket.simulation.SimulationOptions;
 import net.sf.openrocket.simulation.exception.SimulationException;
+import net.sf.openrocket.startup.Application;
 
 
 public class OpenRocketAPI {
 	
-	private FlightData m_CFlightData;
-	private SimulationConditions m_CSimulationConditions;
+	private FlightData m_CFlightData = null;
+	private SimulationConditions m_CSimulationConditions = null;
 	
 	//private SimulationStatus m_CStatus; //will be used later.
 	
+	public int GetVelocityX() {
+		return (int) Math.random();
+	}
+	
+	public int GetVelocityY() {
+		return (int) Math.random();
+	}
+	
+	public int GetVelocityZ() {
+		return (int) Math.random();
+	}
+	
+	
 	public int LoadRocket(String szFileName) {
 		try {
+			//Application.setInjector(Guice.createInjector(new MyServletModule(),
+			//        new BusinessLogicModule());
+			//));
+			
 			File Filename = new File(szFileName);
+			System.out.println(szFileName);
+			
+			Application foo = new Application();
+			
 			GeneralRocketLoader test = new GeneralRocketLoader(Filename);
 			OpenRocketDocument temp = test.load();
+			
 			if (temp.getSimulationCount() > 0)
 			{
 				SimulationOptions temp2 = temp.getSimulation(0).getSimulatedConditions();
@@ -55,38 +78,56 @@ public class OpenRocketAPI {
 	};
 	
 	public double getMaxAltitude() {
+		if (m_CFlightData == null)
+			return -1;
 		return m_CFlightData.getMaxAltitude();
 	}
 	
 	public double getMaxVelocity() {
+		if (m_CFlightData == null)
+			return -1;
 		return m_CFlightData.getMaxVelocity();
 	}
 	
 	public double getMaxAcceleration() {
+		if (m_CFlightData == null)
+			return -1;
 		return m_CFlightData.getTimeToApogee();
 	}
 	
 	public double getMaxMachNumber() {
+		if (m_CFlightData == null)
+			return -1;
 		return m_CFlightData.getMaxMachNumber();
 	}
 	
 	public double getTimeToApogee() {
+		if (m_CFlightData == null)
+			return -1;
 		return m_CFlightData.getTimeToApogee();
 	}
 	
 	public double getFlightTime() {
+		if (m_CFlightData == null)
+			return -1;
 		return m_CFlightData.getFlightTime();
 	}
 	
 	public double getGroundHitVelocity() {
+		if (m_CFlightData == null)
+			return -1;
 		return m_CFlightData.getGroundHitVelocity();
 	}
 	
 	public double getLaunchRodVelocity() {
+		if (m_CFlightData == null)
+			return -1;
 		return m_CFlightData.getLaunchRodVelocity();
 	}
 	
 	public double getDeploymentVelocity() {
+		if (m_CFlightData == null)
+			return -1;
 		return m_CFlightData.getDeploymentVelocity();
 	}
 	
