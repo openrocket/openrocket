c52716942cca48e8109b11c09aac7ada8f51ffb0
bejon <nubjub@gmail.com>
2014-02-21 02:01:11 +0000

    RunVsStep provides initial example of using FlightDataStepToCSV
    Refactored swing.src.net.sf.openrocket.utils.CSVFileWriter to package
    swing.src.net.sf.openrocket.utils.csv.*
    Fixed bugs in FlightDataStepToCSV function

---
+++ swing/src/net/sf/openrocket/utils/csv/CSVException.java
@@ -0,0 +1,22 @@
+package net.sf.openrocket.utils.csv;
+
+class CSVException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public CSVException() {
+    }
+
+    public CSVException(Throwable throwable) {
+        super(throwable);
+    }
+
+    public CSVException(String message) {
+        super(message);
+    }
+
+    public CSVException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+
+}
