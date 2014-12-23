45ba0e3bb021f62f49a5439c43596779d3c03313
Craig Earls <enderw88@gmail.com>
2014-12-22 19:39:17 -0800

    Added additional panel for color defaults

---
+++ swing/src/net/sf/openrocket/gui/dialogs/preferences/ColorDefaultsPanel.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package net.sf.openrocket.gui.dialogs.preferences;
+
+import javax.swing.JPanel;
+
+import net.miginfocom.swing.MigLayout;
+
+/**
+ * @author cpearls
+ *
+ */
+public class ColorDefaultsPanel extends JPanel {
+	public ColorDefaultsPanel(){
+		super(new MigLayout("fillx"));
+	}
+}
