eb5321e82c68eb9e459f7593d9cff99f3f91e949
Sampo Niskanen <sampo.niskanen@iki.fi>
2014-12-27 20:20:32 +0200

    Add script editor highlighting using RSyntaxTextArea

--- swing/src/net/sf/openrocket/simulation/extension/impl/ScriptingConfigurator.java
+++ swing/src/net/sf/openrocket/simulation/extension/impl/ScriptingConfigurator.java
@@ -1,19 +1,21 @@
 package net.sf.openrocket.simulation.extension.impl;
 
+import java.awt.Color;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
 import javax.swing.JComponent;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 
 import net.sf.openrocket.document.Simulation;
 import net.sf.openrocket.gui.components.StyledLabel;
 import net.sf.openrocket.gui.components.StyledLabel.Style;
-import net.sf.openrocket.gui.util.GUIUtil;
 import net.sf.openrocket.plugin.Plugin;
 import net.sf.openrocket.simulation.extension.AbstractSwingSimulationExtensionConfigurator;
+
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import org.fife.ui.rtextarea.RTextScrollPane;
 
 @Plugin
 public class ScriptingConfigurator extends AbstractSwingSimulationExtensionConfigurator<ScriptingExtension> {
@@ -27,11 +29,13 @@ public class ScriptingConfigurator extends AbstractSwingSimulationExtensionConfi
 		
 		panel.add(new StyledLabel(trans.get("SimulationExtension.scripting.script.label"), Style.BOLD), "wrap");
 		
-		final JTextArea text = new JTextArea(extension.getScript(), 20, 60);
+		final RSyntaxTextArea text = new RSyntaxTextArea(extension.getScript(), 15, 60);
+		text.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVASCRIPT);
+		text.setCodeFoldingEnabled(true);
 		text.setLineWrap(true);
 		text.setWrapStyleWord(true);
 		text.setEditable(true);
-		GUIUtil.setTabToFocusing(text);
+		text.setCurrentLineHighlightColor(new Color(255, 255, 230));
 		text.addFocusListener(new FocusListener() {
 			@Override
 			public void focusGained(FocusEvent event) {
@@ -46,7 +50,8 @@ public class ScriptingConfigurator extends AbstractSwingSimulationExtensionConfi
 			}
 		});
 		
-		panel.add(new JScrollPane(text), "grow");
+		RTextScrollPane scroll = new RTextScrollPane(text);
+		panel.add(scroll, "grow");
 		
 		return panel;
 	}
