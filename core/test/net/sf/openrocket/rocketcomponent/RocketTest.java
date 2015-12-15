e89754bbb260bd40bd50acb90ee2bf058bf65f8c
Kevin Ruland <kruland2607@github.com>
2015-12-14 21:45:48 -0600

    Fix RocketTest.testCopyFrom.

--- core/test/net/sf/openrocket/rocketcomponent/RocketTest.java
+++ core/test/net/sf/openrocket/rocketcomponent/RocketTest.java
@@ -3,32 +3,28 @@ package net.sf.openrocket.rocketcomponent;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
-import net.sf.openrocket.material.Material;
-import net.sf.openrocket.rocketcomponent.RocketComponent.Position;
-import net.sf.openrocket.startup.Application;
+
+import org.junit.Test;
+
 import net.sf.openrocket.util.Coordinate;
 import net.sf.openrocket.util.MathUtil;
 import net.sf.openrocket.util.TestRockets;
 import net.sf.openrocket.util.BaseTestCase.BaseTestCase;
 
-import org.junit.Test;
-
 public class RocketTest extends BaseTestCase {
 	
 	@Test
 	public void testCopyFrom() {
-//		Rocket r1 = net.sf.openrocket.util.TestRockets.makeIsoHaisu();
-//		Rocket r2 = net.sf.openrocket.util.TestRockets.makeBigBlue();
-//		
-//		Rocket copy = (Rocket) r2.copy();
-//		
-//		ComponentCompare.assertDeepEquality(r2, copy);
-//		
-//		r1.copyFrom(copy);
-//		
-//		ComponentCompare.assertDeepEquality(r1, r2);
-		fail("NYI");
+		Rocket r1 = net.sf.openrocket.util.TestRockets.makeIsoHaisu();
+		Rocket r2 = net.sf.openrocket.util.TestRockets.makeBigBlue();
+		
+		Rocket copy = (Rocket) r2.copy();
+		
+		ComponentCompare.assertDeepEquality(r2, copy);
+		
+		r1.copyFrom(copy);
+		
+		ComponentCompare.assertDeepEquality(r1, r2);
 	}
 
 	@Test
