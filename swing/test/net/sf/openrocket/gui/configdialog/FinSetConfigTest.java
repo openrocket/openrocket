b749be1a3479a97844c3706318f865d9b7a7a189
MadCompSci <Spamwall@charter.net>
2016-09-29 15:09:00 -0500

    Whitespace changes only.

--- swing/test/net/sf/openrocket/gui/configdialog/FinSetConfigTest.java
+++ swing/test/net/sf/openrocket/gui/configdialog/FinSetConfigTest.java
@@ -1,5 +1,13 @@
 package net.sf.openrocket.gui.configdialog;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,10 +17,6 @@ import net.sf.openrocket.rocketcomponent.BodyTube;
 import net.sf.openrocket.rocketcomponent.CenteringRing;
 import net.sf.openrocket.rocketcomponent.RocketComponent;
 import net.sf.openrocket.util.BaseTestCase.BaseTestCase;
-
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 public class FinSetConfigTest extends BaseTestCase {
 
@@ -68,7 +72,7 @@ public class FinSetConfigTest extends BaseTestCase {
 
         Double result = (Double)method.invoke(null, rings, 0.47d, 0.01, dm, parent);
         Assert.assertEquals(0.01, result.doubleValue(), 0.0001);
-        
+
     }
 
     /**
@@ -168,7 +172,7 @@ public class FinSetConfigTest extends BaseTestCase {
         Double result = (Double)method.invoke(null, rings, 0.47d, 0.01, dm, parent);
         Assert.assertEquals(0.0059, result.doubleValue(), 0.0001);
     }
-    
+
     /**
      * Test both rings within the root chord.
      */
