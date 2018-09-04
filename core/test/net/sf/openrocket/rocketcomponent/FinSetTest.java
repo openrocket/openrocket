8dc851e1db5098ac77c10b4e8178dd9bb9e874c5
Daniel_M_Williams <equipoise@gmail.com>
2018-09-03 22:51:00 -0400

    [fix] cleanup up unused imports in core/test/net/sf/openrocket/rocketcomponent/*

--- core/test/net/sf/openrocket/rocketcomponent/FinSetTest.java
+++ core/test/net/sf/openrocket/rocketcomponent/FinSetTest.java
@@ -1,24 +1,9 @@
 package net.sf.openrocket.rocketcomponent;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import net.sf.openrocket.aerodynamics.AerodynamicForces;
-import net.sf.openrocket.aerodynamics.FlightConditions;
-import net.sf.openrocket.aerodynamics.WarningSet;
-import net.sf.openrocket.aerodynamics.barrowman.FinSetCalc;
-import net.sf.openrocket.material.Material;
-import net.sf.openrocket.material.Material.Type;
-import net.sf.openrocket.rocketcomponent.ExternalComponent.Finish;
-import net.sf.openrocket.rocketcomponent.FinSet.CrossSection;
-import net.sf.openrocket.rocketcomponent.FinSet.TabRelativePosition;
-import net.sf.openrocket.rocketcomponent.position.*;
-import net.sf.openrocket.util.Color;
-import net.sf.openrocket.util.Coordinate;
-import net.sf.openrocket.util.LineStyle;
 import net.sf.openrocket.util.BaseTestCase.BaseTestCase;
 
 public class FinSetTest extends BaseTestCase {
