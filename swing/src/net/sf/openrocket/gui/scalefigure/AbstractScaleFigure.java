7b81f763c7f99e191861b828d1c02e51f6405749
Daniel_M_Williams <equipoise@gmail.com>
2020-06-20 14:31:29 -0400

    [refactor] ComponentAssembly components no longer have bounds -- it's calculated from individual components, on a per-instance basis.

--- swing/src/net/sf/openrocket/gui/scalefigure/AbstractScaleFigure.java
+++ swing/src/net/sf/openrocket/gui/scalefigure/AbstractScaleFigure.java
@@ -45,30 +45,30 @@ public abstract class AbstractScaleFigure extends JPanel {
 	// size of the visible region
 	protected Dimension visibleBounds_px = new Dimension(0,0);
 	
-    // ======= whatever this figure is drawing, in real-space coordinates:  meters
-    protected Rectangle2D subjectBounds_m = null;
-    
-    // combines the translation and scale in one place: 
-    // which frames does this transform between ?  
-    protected AffineTransform projection = null;
+	// ======= whatever this figure is drawing, in real-space coordinates:  meters
+	protected Rectangle2D subjectBounds_m = null;
+
+	// combines the translation and scale in one place:
+	// which frames does this transform between ?
+	protected AffineTransform projection = null;
 
 	protected final List<EventListener> listeners = new LinkedList<EventListener>();
 	
 	
 	public AbstractScaleFigure() {
-	    // produces a pixels-per-meter scale factor 
-	    //
-	    // dots     dots     inch
-	    // ----  = ------ * -----
-	    // meter    inch    meter
-	    //
-        this.baseScale = GUIUtil.getDPI() * INCHES_PER_METER;
-        this.userScale = 1.0;
-        this.scale = baseScale * userScale;
- 
-        this.setPreferredSize(new Dimension(100,100));
-        setSize(100,100);
-        
+		// produces a pixels-per-meter scale factor
+		//
+		// dots     dots     inch
+		// ----  = ------ * -----
+		// meter    inch    meter
+		//
+		this.baseScale = GUIUtil.getDPI() * INCHES_PER_METER;
+		this.userScale = 1.0;
+		this.scale = baseScale * userScale;
+
+		this.setPreferredSize(new Dimension(100,100));
+		setSize(100,100);
+
 		setBackground(Color.WHITE);
 		setOpaque(true);
 	}
@@ -77,28 +77,28 @@ public abstract class AbstractScaleFigure extends JPanel {
 		return userScale;
 	}
 	
-    public double getAbsoluteScale() {
-       return scale;
-    }
+	public double getAbsoluteScale() {
+		return scale;
+	}
 
-    public Dimension getSubjectOrigin() {
+	public Dimension getSubjectOrigin() {
         return originLocation_px;
     }
-    
+
 	/**
 	 * Set the scale level of the figure.  A scale value of 1.0 is equivalent to 100 % scale.
 	 * Smaller scales display the subject smaller.
 	 *
 	 *  If the figure would be smaller than the 'visibleBounds', then the figure is grown to match,
 	 *  and the figures internal contents are centered according to the figure's origin.
-	 * 
+	 *
 	 * @param newScaleRequest the scale level
 	 * @param visibleBounds the visible bounds upon the Figure
 	 */
 	public void scaleTo(final double newScaleRequest, final Dimension visibleBounds) {
 		if (MathUtil.equals(this.userScale, newScaleRequest, 0.01)){
 			return;}
-		if (Double.isInfinite(newScaleRequest) || Double.isNaN(newScaleRequest)) {
+		if (Double.isInfinite(newScaleRequest) || Double.isNaN(newScaleRequest) || 0 > newScaleRequest) {
 			return;}
 
 		this.userScale = MathUtil.clamp( newScaleRequest, MINIMUM_ZOOM, MAXIMUM_ZOOM);
@@ -115,21 +115,21 @@ public abstract class AbstractScaleFigure extends JPanel {
      * @param visibleBounds the visible bounds to scale this figure to.  
      */
 	public void scaleTo(Dimension visibleBounds) {
-	    if( 0 == visibleBounds.getWidth() || 0 == visibleBounds.getHeight())
-	        return;
-	    
-	    updateSubjectDimensions();
-	    
-	    // dimensions within the viewable area, which are available to draw
+		if( 0 == visibleBounds.getWidth() || 0 == visibleBounds.getHeight())
+			return;
+
+		updateSubjectDimensions();
+
+		// dimensions within the viewable area, which are available to draw
 		final int drawable_width_px = visibleBounds.width - 2 * borderThickness_px.width;
 		final int drawable_height_px = visibleBounds.height - 2 * borderThickness_px.height;
 
-        if(( 0 < drawable_width_px ) && ( 0 < drawable_height_px)) {
-		    final double width_scale = (drawable_width_px) / ( subjectBounds_m.getWidth() * baseScale);
-    		final double height_scale = (drawable_height_px) / ( subjectBounds_m.getHeight() * baseScale);
-    		final double minScale = Math.min(height_scale, width_scale);
-    		
-    		scaleTo(minScale, visibleBounds);
+		if(( 0 < drawable_width_px ) && ( 0 < drawable_height_px)) {
+			final double width_scale = (drawable_width_px) / ( subjectBounds_m.getWidth() * baseScale);
+			final double height_scale = (drawable_height_px) / ( subjectBounds_m.getHeight() * baseScale);
+			final double minScale = Math.min(height_scale, width_scale);
+
+			scaleTo(minScale, visibleBounds);
 		}
 	}
 	
