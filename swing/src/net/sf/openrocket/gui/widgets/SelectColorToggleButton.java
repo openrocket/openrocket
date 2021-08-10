2f6a8c8db94c80c5a57ef381db34d97c98fbd50f
Sibo Van Gool <sibo.vangool@hotmail.com>
2021-08-10 19:35:34 +0200

    [fixes #989] Use changelistener for foreground change + don't change on non OSX-devices

--- swing/src/net/sf/openrocket/gui/widgets/SelectColorToggleButton.java
+++ swing/src/net/sf/openrocket/gui/widgets/SelectColorToggleButton.java
@@ -4,7 +4,12 @@ import javax.swing.JToggleButton;
 import javax.swing.Action;
 import javax.swing.Icon;
 import javax.swing.UIManager;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 
 /**
  * This class is a replacement for the standard JToggleButton. Its purpose is to be able
@@ -15,43 +20,57 @@ import java.awt.Graphics;
 public class SelectColorToggleButton extends JToggleButton {
     public SelectColorToggleButton(Action a) {
         super(a);
+        addChangeListenerSelectColor();
     }
 
     public SelectColorToggleButton(String text) {
         super(text);
+        addChangeListenerSelectColor();
     }
 
     public SelectColorToggleButton() {
+        addChangeListenerSelectColor();
     }
 
     public SelectColorToggleButton(Icon icon) {
         super(icon);
+        addChangeListenerSelectColor();
     }
 
     public SelectColorToggleButton(Icon icon, boolean selected) {
         super(icon, selected);
+        addChangeListenerSelectColor();
     }
 
     public SelectColorToggleButton(String text, boolean selected) {
         super(text, selected);
+        addChangeListenerSelectColor();
     }
 
     public SelectColorToggleButton(String text, Icon icon) {
         super(text, icon);
+        addChangeListenerSelectColor();
     }
 
     public SelectColorToggleButton(String text, Icon icon, boolean selected) {
         super(text, icon, selected);
+        addChangeListenerSelectColor();
     }
 
-    @Override
-    public void paint(Graphics g) {
-        if (isSelected()) {
-            setForeground(UIManager.getColor("ToggleButton.selectForeground"));
-        }
-        else {
-            setForeground(UIManager.getColor("ToggleButton.foreground"));
-        }
-        super.paint(g);
+    private void addChangeListenerSelectColor() {
+        addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                if (UIManager.getColor("ToggleButton.selectForeground") == null
+                        || UIManager.getColor("ToggleButton.foreground") == null)
+                    return;
+                if (isSelected()) {
+                    setForeground(UIManager.getColor("ToggleButton.selectForeground"));
+                }
+                else {
+                    setForeground(UIManager.getColor("ToggleButton.foreground"));
+                }
+            }
+        });
     }
 }
