2231c995c2b85234568b4bc88ecefda0bbec18e4
Sibo Van Gool <sibo.vangool@hotmail.com>
2021-09-10 01:04:35 +0200

    [fixes #899] Remove double overwrite popup in export dialog

--- swing/src/net/sf/openrocket/gui/main/BasicFrame.java
+++ swing/src/net/sf/openrocket/gui/main/BasicFrame.java
@@ -1410,9 +1410,6 @@ public class BasicFrame extends JFrame {
 				return false;
 			}
 		}
-		if (!FileHelper.confirmWrite(file, this)) {
-			return false;
-		}
 
 		try {
 			StorageOptions options = new StorageOptions();
