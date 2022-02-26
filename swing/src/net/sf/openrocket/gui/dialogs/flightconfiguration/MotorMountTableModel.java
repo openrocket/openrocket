05aae619322060693d6c48a751eb35fe22e8efaa
thzero <corvette@thzero.com>
2022-02-26 09:47:57 -0600

    Refactored from new Boolean and new Integer to Boolean.valueOf and Integer.valueOf due to deprecation warnings.

--- swing/src/net/sf/openrocket/gui/dialogs/flightconfiguration/MotorMountTableModel.java
+++ swing/src/net/sf/openrocket/gui/dialogs/flightconfiguration/MotorMountTableModel.java
@@ -78,7 +78,7 @@ class MotorMountTableModel extends AbstractTableModel implements ComponentChange
 	public Object getValueAt(int row, int column) {
 		switch (column) {
 		case 0:
-			return new Boolean(potentialMounts.get(row).isMotorMount());
+			return Boolean.valueOf(potentialMounts.get(row).isMotorMount());
 			
 		case 1:
 			return potentialMounts.get(row).toString();
