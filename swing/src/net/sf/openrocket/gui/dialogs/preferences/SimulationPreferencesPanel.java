70f3d0fa12a41f6a40c2668c918faf1510761aae
Remington Holder <remingtonholder@protonmail.com>
2022-05-29 13:51:10 -0400

    Small UI Fix To Update ComboBox To Display Selection (#1367)

--- swing/src/net/sf/openrocket/gui/dialogs/preferences/SimulationPreferencesPanel.java
+++ swing/src/net/sf/openrocket/gui/dialogs/preferences/SimulationPreferencesPanel.java
@@ -115,6 +115,7 @@ public class SimulationPreferencesPanel extends PreferencesPanel {
 				GeodeticComputationStrategy gcs = (GeodeticComputationStrategy) gcsCombo
 						.getSelectedItem();
 				gcsCombo.setToolTipText(gcs.getDescription());
+				gcsCombo.repaint(); // On some machines, the combobox did not visually update to the selected item
 			}
 		};
 		gcsCombo.addActionListener(gcsTTipListener);
