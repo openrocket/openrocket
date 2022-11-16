0056c53640ee05f4daeb1581e27bbec84859d337
SiboVG <sibo.vangool@hotmail.com>
2022-11-17 00:48:39 +0100

    [#1829] Fix tesselation issue on fins with curved root

--- core/src/net/sf/openrocket/rocketcomponent/FinSet.java
+++ core/src/net/sf/openrocket/rocketcomponent/FinSet.java
@@ -33,7 +33,7 @@ public abstract class FinSet extends ExternalComponent implements AxialPositiona
 	 * Maximum number of root points in the root geometry.
 	 */
 	private static final int MAX_ROOT_DIVISIONS = 100;
-	private static final int MAX_ROOT_DIVISIONS_LOW_RES = 15;
+	private static final int MAX_ROOT_DIVISIONS_LOW_RES = MAX_ROOT_DIVISIONS / 5;
 
     public void setOverrideMass() {
     }
