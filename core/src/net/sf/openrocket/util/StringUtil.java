91332baafaf42eb34c71d7fe3ba925c1a9bb5118
SiboVG <sibo.vangool@hotmail.com>
2022-11-17 12:43:24 +0100

    [#1842] More general string conversion to double for fin editor table

--- core/src/net/sf/openrocket/util/StringUtil.java
+++ core/src/net/sf/openrocket/util/StringUtil.java
@@ -19,5 +19,24 @@ public class StringUtil {
 		}
 		return "".equals(s.trim());
 	}
+
+	/**
+	 * Converts a string to a double, but with a more robust locale handling.
+	 * Some systems use a comma as a decimal separator, some a dot. This method
+	 * should work for both cases
+	 * @param input string to convert
+	 * @return double converted from string
+	 * @throws NumberFormatException   if the string cannot be parsed.
+	 */
+	public static double convertToDouble(String input) {
+		input = input.replace(',', '.');
+		int decimalSeparator = input.lastIndexOf('.');
+
+		if (decimalSeparator > -1) {
+			input = input.substring(0, decimalSeparator).replace(".", "") + input.substring(decimalSeparator);
+		}
+
+		return Double.parseDouble(input);
+	}
 	
 }
