b718c5a0e1ad222376ff5cb4dfc6a230aacc0712
SiboVG <sibo.vangool@hotmail.com>
2023-02-01 15:51:49 +0000

    [#1999] Add checkbox to show/hide warnings

--- swing/src/net/sf/openrocket/gui/figureelements/RocketInfo.java
+++ swing/src/net/sf/openrocket/gui/figureelements/RocketInfo.java
@@ -53,6 +53,7 @@ public class RocketInfo implements FigureElement {
 	private double massWithoutMotors = 0;
 	
 	private WarningSet warnings = null;
+	private boolean showWarnings = true;
 	
 	private boolean calculatingData = false;
 	private FlightData flightData = null;
@@ -123,7 +124,15 @@ public class RocketInfo implements FigureElement {
 	public void setWarnings(WarningSet warnings) {
 		this.warnings = warnings.clone();
 	}
-	
+
+	/**
+	 * Set whether warnings should be shown. If false, the warnings are not shown.
+	 * @param showWarnings whether to show warnings.
+	 */
+	public void setShowWarnings(boolean showWarnings) {
+		this.showWarnings = showWarnings;
+	}
+
 	public void setAOA(double aoa) {
 		this.aoa = aoa;
 	}
@@ -329,25 +338,34 @@ public class RocketInfo implements FigureElement {
 		if (warnings == null || warnings.isEmpty())
 			return;
 		
-		GlyphVector[] texts = new GlyphVector[warnings.size()+1];
+		final GlyphVector[] texts;
 		double max = 0;
-		
-		//// Warning:
-		texts[0] = createText(trans.get("RocketInfo.Warning"));
-		int i=1;
-		for (Warning w: warnings) {
-			texts[i] = createText(w.toString());
-			i++;
-		}
-		
-		for (GlyphVector v: texts) {
-			Rectangle2D rect = v.getVisualBounds();
-			if (rect.getWidth() > max)
-				max = rect.getWidth();
+
+		if (showWarnings) {
+			texts = new GlyphVector[warnings.size()+1];
+
+			//// Warning:
+			texts[0] = createText(trans.get("RocketInfo.Warning"));
+			int i = 1;
+			for (Warning w : warnings) {
+				texts[i] = createText(w.toString());
+				i++;
+			}
+
+			for (GlyphVector v : texts) {
+				Rectangle2D rect = v.getVisualBounds();
+				if (rect.getWidth() > max)
+					max = rect.getWidth();
+			}
+		} else {
+			texts = new GlyphVector[1];
+			texts[0] = createText(String.format(trans.get("RocketInfo.lbl.warnings"), warnings.size()));
+			Rectangle2D rect = texts[0].getVisualBounds();
+			max = rect.getWidth();
 		}
 		
 
-		float y = y2 - line * warnings.size();
+		float y = y2 - line * (texts.length-1);
 		g2.setColor(Color.RED);
 
 		for (GlyphVector v: texts) {
