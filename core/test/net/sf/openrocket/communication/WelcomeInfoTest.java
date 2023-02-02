a41210014047577e508d1723a7144abdbb5283db
SiboVG <sibo.vangool@hotmail.com>
2023-02-02 02:20:02 +0000

    Add additional debug messages

--- core/test/net/sf/openrocket/communication/WelcomeInfoTest.java
+++ core/test/net/sf/openrocket/communication/WelcomeInfoTest.java
@@ -17,8 +17,8 @@ public class WelcomeInfoTest extends BaseTestCase {
     public void testWelcomeInfo() throws Exception {
         // Test the welcome info for the current build version
         String welcomeInfo = WelcomeInfoRetriever.retrieveWelcomeInfo();
-        assertNotNull(welcomeInfo);
-        assertTrue(welcomeInfo.length() > 0);
+        assertNotNull("Current release version not present in release notes", welcomeInfo);
+        assertTrue("Body of release notes is empty", welcomeInfo.length() > 0);
 
         // Test the release info for a bogus release version
         welcomeInfo = WelcomeInfoRetriever.retrieveWelcomeInfo("bogus release");
