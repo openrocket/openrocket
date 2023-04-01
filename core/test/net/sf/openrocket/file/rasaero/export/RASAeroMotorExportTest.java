153bd9ebf9aed90d46e6d28e581254ef9d101418
SiboVG <sibo.vangool@hotmail.com>
2023-04-01 10:06:31 +0200

    TODO: implement unit testing

---
+++ core/test/net/sf/openrocket/file/rasaero/export/RASAeroMotorExportTest.java
@@ -0,0 +1,6 @@
+package net.sf.openrocket.file.rasaero.export;
+
+public class RASAeroMotorExportTest {
+    // TODO: check correct name after export
+    // TODO: check 0-delay motors (should have -0 suffix)
+}
---
+++ core/test/net/sf/openrocket/file/rasaero/export/RASAeroSaverTest.java
@@ -0,0 +1,7 @@
+package net.sf.openrocket.file.rasaero.export;
+
+public class RASAeroSaverTest {
+    // TODO: export a complex design
+    // TODO: check recovery
+    // TODO: check sims (including weights and CG)
+}
