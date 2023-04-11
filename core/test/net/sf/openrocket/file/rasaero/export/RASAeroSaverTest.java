884959a98e1ef55dddc9537ffd39e4e70a105db7
SiboVG <sibo.vangool@hotmail.com>
2023-04-11 16:34:22 +0200

    Need at least one runnable unit test

--- core/test/net/sf/openrocket/file/rasaero/export/RASAeroSaverTest.java
+++ core/test/net/sf/openrocket/file/rasaero/export/RASAeroSaverTest.java
@@ -1,7 +1,14 @@
 package net.sf.openrocket.file.rasaero.export;
 
+import org.junit.Test;
+
 public class RASAeroSaverTest {
     // TODO: export a complex design
     // TODO: check recovery
     // TODO: check sims (including weights and CG)
+
+    @Test
+    public void dummy() {
+        // We need at least one runnable test method
+    }
 }
