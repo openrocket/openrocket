2123531f14a1b7c95d222af7861ae84abceb3744
JoePfeiffer <joseph@pfeifferfamily.net>
2023-12-24 06:20:21 -0700

    Create new SIM_ABORT FlightEvent

--- core/src/net/sf/openrocket/simulation/FlightEvent.java
+++ core/src/net/sf/openrocket/simulation/FlightEvent.java
@@ -1,6 +1,7 @@
 package net.sf.openrocket.simulation;
 
 import net.sf.openrocket.l10n.Translator;
+import net.sf.openrocket.logging.SimulationAbort;
 import net.sf.openrocket.rocketcomponent.AxialStage;
 import net.sf.openrocket.rocketcomponent.MotorMount;
 import net.sf.openrocket.rocketcomponent.RocketComponent;
@@ -80,9 +81,15 @@ public class FlightEvent implements Comparable<FlightEvent> {
 		 * The rocket begins to tumble.
 		 */
 		TUMBLE(trans.get("FlightEvent.Type.TUMBLE")),
+
+		/**
+		 * It is impossible for the simulation proceed due to characteristics
+		 * of the rocket or flight configuration
+		 */
+		SIM_ABORT(trans.get("FlightEvent.Type.SIM_ABORT")),
 		
 		/**
-		 * Simulation aborted
+		 * Simulation exception thrown due to error in OpenRocket code
 		 */
 		EXCEPTION(trans.get("FlightEvent.Type.EXCEPTION"));
 		
@@ -237,6 +244,11 @@ public class FlightEvent implements Comparable<FlightEvent> {
 					throw new IllegalStateException(type.name()+" events should have "
 							+MotorClusterState.class.getSimpleName()+" type data payloads");
 				}
+			}
+			break;
+		case SIM_ABORT:
+			if (( null == this.data ) || ( ! ( this.data instanceof SimulationAbort ))) {
+				throw new IllegalStateException(type.name()+" events require SimulationAbort objects");
 			}
 			break;
 		case LAUNCH:
