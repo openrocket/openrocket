d3caaddde95b0a718c286748c547200071432607
SiboVG <sibo.vangool@hotmail.com>
2024-11-21 22:47:01 +0100

    [#2597] Remove double semicolons in code

--- core/src/test/java/info/openrocket/core/rocketcomponent/NoseConeTest.java
+++ core/src/test/java/info/openrocket/core/rocketcomponent/NoseConeTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertSame;;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NoseConeTest extends BaseTestCase {
