5545faceeabcf9c0f50b32642ff1e65e43ac4077
JoePfeiffer <joseph@pfeifferfamily.net>
2025-02-10 13:25:02 -0700

    back out fix for 2675

--- core/src/main/java/info/openrocket/core/simulation/BasicEventSimulationEngine.java
+++ core/src/main/java/info/openrocket/core/simulation/BasicEventSimulationEngine.java
@@ -146,22 +146,13 @@ public class BasicEventSimulationEngine implements SimulationEngine {
 	
 	private void simulateLoop(SimulationConditions simulationConditions) throws SimulationException {
 		// Initialize the simulation.
-
-		// Select the appropriate stepper:
-		//     On the ground: ground stepper
-		//     Tumbling: tumble stepper
-		//     At least one recovery device deployed: landing stepper
-		//     Otherwise: flight stepper
-
-		if (currentStatus.isLanded()) {
+		
+		// Initialize the simulation. We'll use the flight stepper unless we're already
+		// on the ground.
+		if (currentStatus.isLanded())
 			currentStepper = groundStepper;
-		} else if (currentStatus.isTumbling()) {
-			currentStepper = tumbleStepper;
-		} else if (!currentStatus.getDeployedRecoveryDevices().isEmpty()) {
-			currentStepper = landingStepper;
-		} else {
+		else
 			currentStepper = flightStepper;
-		}
 
 		currentStatus = currentStepper.initialize(currentStatus);
 		double previousSimulationTime = currentStatus.getSimulationTime();
