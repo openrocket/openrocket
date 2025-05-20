c95db018b867c544a9827f3959486f4dd37cc62c
senft-research <JordanSenft@proton.me>
2025-05-20 11:52:57 +0100

    - Documentation of the new OptionChooser.java.

--- swing/src/main/java/info/openrocket/swing/gui/choosers/OptionChooser.java
+++ swing/src/main/java/info/openrocket/swing/gui/choosers/OptionChooser.java
@@ -3,6 +3,15 @@ package info.openrocket.swing.gui.choosers;
 import info.openrocket.core.document.OpenRocketDocument;
 import info.openrocket.core.preferences.ApplicationPreferences;
 
+/**
+ * Interface representing any instance that is responsible for the Options chosen within an OpenRocketDocument. Examples
+ * include {@link OBJOptionChooser} & {@link StorageOptionChooser}.
+ */
 public interface OptionChooser {
+    /**
+     * Store the options this chooser is responsible for to the specified document.
+     * @param document The document to store the options for.
+     * @param preferences Application preferences that may need alteration due to the newly stored options.
+     */
     void storeOptions(OpenRocketDocument document, ApplicationPreferences preferences);
 }
