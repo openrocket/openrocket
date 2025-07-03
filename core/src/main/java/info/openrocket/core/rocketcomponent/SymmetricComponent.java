f814b48da249d599c6ace1da571d9e6937ee2ecb
SiboVG <sibo.vangool@hotmail.com>
2025-07-03 21:31:01 +0200

    Remove sketchy import

--- core/src/main/java/info/openrocket/core/rocketcomponent/SymmetricComponent.java
+++ core/src/main/java/info/openrocket/core/rocketcomponent/SymmetricComponent.java
@@ -9,7 +9,6 @@ import info.openrocket.core.rocketcomponent.position.AxialMethod;
 import info.openrocket.core.util.BoundingBox;
 import info.openrocket.core.util.Coordinate;
 import info.openrocket.core.util.MathUtil;
-import org.checkerframework.checker.units.qual.C;
 
 import static info.openrocket.core.util.MathUtil.pow2;
 
