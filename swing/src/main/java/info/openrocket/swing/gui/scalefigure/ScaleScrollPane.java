2ac53a45788da80d49eb38ffeaa945504e1bde2a
SiboVG <sibo.vangool@hotmail.com>
2026-04-10 17:58:23 +0200

    Add missing import

--- swing/src/main/java/info/openrocket/swing/gui/scalefigure/ScaleScrollPane.java
+++ swing/src/main/java/info/openrocket/swing/gui/scalefigure/ScaleScrollPane.java
@@ -27,6 +27,7 @@ import javax.swing.event.ChangeListener;
 import info.openrocket.swing.gui.adaptors.DoubleModel;
 import info.openrocket.swing.gui.components.UnitSelector;
 import info.openrocket.swing.gui.theme.UITheme;
+import info.openrocket.swing.gui.util.GUIUtil;
 import info.openrocket.core.unit.Tick;
 import info.openrocket.core.unit.Unit;
 import info.openrocket.core.unit.UnitGroup;
